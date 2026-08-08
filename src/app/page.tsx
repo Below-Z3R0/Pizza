35ce24662aefd152ba4df641d012cbd8e9188979
Below-Z3R0 <macen@centenoadvisory.com>
2026-08-08 12:09:06 -0500

    fix: force dynamic rendering to avoid prerender error on Vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useState } from "react";
+
+export const dynamic = "force-dynamic";
 import { useDashboard } from "@/hooks/useDashboard";
 import { DashboardShell } from "@/components/organisms/DashboardShell";
 import { DashboardHeader } from "@/components/organisms/DashboardHeader";
